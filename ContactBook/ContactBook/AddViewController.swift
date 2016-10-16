5f0ad0e2fdd4223c6eaf16947f88ffe715af741c
Noel Mowatt <nwmowatt@fullsail.edu>
2016-10-16 18:07:01 -0400

    Setting up AddVC
    -Added and constrained UI elements on AddVC
    -Added segue from MasterVC to AddVC
    -Connected UI elements from AddVC to code

--- ContactBook/ContactBook/AddViewController.swift
+++ ContactBook/ContactBook/AddViewController.swift
@@ -10,7 +10,18 @@ import Foundation
 import UIKit
 
 //--Define new class
+/*  This view controller will be displayed when i want to add
+    a new contact to my contact book */
+
 class AddViewController: UIViewController{
+    
+    //--Outlet name field 
+    @IBOutlet weak var nameField: UITextField!
+    
+    //--Add button func
+    @IBAction func AddButtonPressed(sender: UIButton){
+        print("Add button pressed")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
