f14a80edd1f701cc0ee239aa491c8c25e93ee55e
Noel Mowatt <nwmowatt@fullsail.edu>
2016-10-17 08:27:36 -0400

    Updated Person Class
    -Made properties of Person class private set
    -Cretated a set first name func to allow setting

--- ContactBook/ContactBook/Person.swift
+++ ContactBook/ContactBook/Person.swift
@@ -8,16 +8,29 @@
 
 import Foundation
 
+//--Person validation error
+enum PersonValidationError: Error{
+    
+}
+
 //--Represents each person in contact book
 class Person{
-    var fName: String?
-    var lName: String?
-    var myAddress: String?
-    var myPhone: String?
-    var myEmail: String?
+    
+    //--made properties private to validate data before
+    //--they are set and read outside the class
+    private(set) var fName: String?
+    private(set) var lName: String?
+    private(set) var myAddress: String?
+    private(set) var myPhone: String?
+    private(set) var myEmail: String?
     
     //--initializer
     init(fName fn: String) {
+        fName = fn
+    }
+    
+    //--Allows outside world to set the properties
+    func setFirstName(fn: String) {
         fName = fn
     }
 }
